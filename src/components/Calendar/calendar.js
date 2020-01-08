38ce3dd857c3e92d207e87ab60cfc3ba6174ead5
Dzmitry <Sakalouski>
2020-01-08 12:59:41 +0300

    fix calendar selected range

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -3,17 +3,21 @@ import Calendar from 'react-calendar/dist/entry.nostyle';
 import './styles.scss';
 
 export const CalendarComponent = ({ classes }) => {
-    const newdays = [new Date()];
+    const newdays = [new Date(2020, 0, 7), new Date(2020, 0, 5), new Date()];
 
     const isDateInArray = date => {
         const selectedDate = newdays.find(dateItem => {
+            // console.log(date.toDateString() === dateItem.toDateString())
             return date.toDateString() === dateItem.toDateString();
         })
-
+        // !!selectedDate && console.log("selectedDate =>", selectedDate, !!selectedDate);
         return !!selectedDate;
     }
 
     const setSelectedClassName = ({ activeStartDate, date, view }) => {
+        console.log("setSelectedClassName", date);
+        console.log();
+        console.log("isDateInArray", isDateInArray(date))
         return isDateInArray(date) ? 'selected' : null
     };
   
